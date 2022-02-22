cc1503ad1c4dd36a07b6c243cc42e1946f203c24
Jaime Roman <jaimeromangil@gmail.com>
2022-02-22 15:13:56 +0100

    update Assets

--- Assets/Scripts/Friends/TalkScript.cs
+++ Assets/Scripts/Friends/TalkScript.cs
@@ -3,8 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TalkScript : MonoBehaviour
-{
-    private AudioSource audioSource;
-    void Start(){audioSource = gameObject.GetComponent<AudioSource>();}
-    public void talk(){audioSource.Play();}
+{   
+    private AudioClip next_clip;
+    private List<AudioClip> elapsed_clips;
+    public List<AudioClip> clips;
+    public AudioSource MyaudioSource;
+    void Start(){}
+
+
+    public void talk(){
+        if(clips.Count == 0){
+            clips=elapsed_clips;
+            Debug.Log("Clips Reset");
+        }
+
+        int index = Random.Range(0, clips.Count-1);
+        Debug.Log("Current Clip--> " + clips[index] + " -last Count- " + clips.Count);     
+        Invoke("magia",1);
+        MyaudioSource.clip = clips[index];
+        MyaudioSource.Play();
+    }
+
+    void magia(){
+        Debug.Log("MAGIA");
+        elapsed_clips.Add(next_clip);
+        clips.Remove(next_clip);
+        Debug.Log("Current Clip--> actual Count- " + clips.Count + " elapsed count " + elapsed_clips.Count);
+
+    }
+
+    void OnTriggerEnter(Collider other){
+        Debug.Log("onTriggerEnter");
+        if(other.tag == "Player"){
+            Debug.Log("Player enter");
+            talk();
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        
+    }
+
 }
